4e2e89f6d3601aadbedf477460d80c17582a08c3
ahmad musa abdullah <ahmadmusa9805@gmail.com>
2024-07-09 11:12:14 +0600

    four commit

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cookieParser());
 
 app.get("/", (req: any, res: any) => {
-  let = 10;
+  let = "10";
   res.send("Hello World!");
 });
 
