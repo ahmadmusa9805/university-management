808663d89b125745cf3f0aab42d70ed1671409ca
ahmad musa abdullah <ahmadmusa9805@gmail.com>
2024-07-09 12:40:51 +0600

    setup basic with lint-stage, husky, express, mongoose, etc

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import path from 'path'
-import dotenv from 'dotenv'
+import path from "path";
+import dotenv from "dotenv";
 
-dotenv.config({ path: path.join((process.cwd(), '.env')) })
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
 
 export default {
   port: process.env.PORT,
   db_url: process.env.DB_URL,
-}
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
 
 async function main() {
-  await mongoose.connect(config.db_url as string)
+  await mongoose.connect(config.db_url as string);
 
   app.listen(config.port, () => {
-    console.log(`application is listening on port ${config.port}`)
-  })
+    console.log(`application is listening on port ${config.port}`);
+  });
 }
 
-main()
+main();
